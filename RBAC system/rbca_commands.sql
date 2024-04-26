e641ac4e087eccaae63f4e23e6e0f32dd79b86c2
Karoly <karoly.webdev@gmail.com>
2024-04-26 16:14:15 +0100

    RBAC update

--- RBAC system/rbca_commands.sql
+++ RBAC system/rbca_commands.sql
@@ -1,6 +1,10 @@
+-- Create groups:
 CREATE ROLE p_customers_read WITH NOLOGIN;
 
+-- Grant permissions:
 GRANT SELECT ON customers TO p_customers_read;
 
 -- Create users:
 CREATE ROLE karoly LOGIN PASSWORD 'karoly';
+
+REVOKE SELECT ON customers FROM PUBLIC;
