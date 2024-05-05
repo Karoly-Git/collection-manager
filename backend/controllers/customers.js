15582827111a7454e31ed797a5e3c91cb5c1b005
Karoly <karoly.webdev@gmail.com>
2024-05-05 17:25:46 +0100

    Updata

--- backend/controllers/customers.js
+++ backend/controllers/customers.js
@@ -1,4 +1,5 @@
-const customersData = require("../models/db/customers.json");
+const fs = require("fs");
+const customersData = require("../models/db/customers_test.json");
 
 const getAllCustomers = (req, res) => {
     try {
@@ -30,4 +31,39 @@ const getCustomerById = (req, res) => {
     }
 };
 
-module.exports = { getAllCustomers, getCustomerById };
+const updateCustomerById = (req, res) => {
+    try {
+        const { customer } = req.body;
+        const { id } = req.params;
+
+        if (!customer) {
+            res.status(400).json({ error: "Invalid object" });
+        } else {
+            let index = customersData.findIndex((element) => element.id === Number(id));
+
+            if (index === -1) {
+                res.status(404).json({ error: "Customer not found" });
+            } else {
+                customersData[index] = { id: Number(id), ...customer };
+                res.status(200).json(customersData[index]);
+                const jsonString = JSON.stringify(customersData, null, 2);
+                fs.writeFile("./models/db/customers_test.json", jsonString, "utf-8", (err) => {
+                    if (err) {
+                        console.error("Error writing JSON file:", err);
+                        return;
+                    }
+                    console.log("JSON file written successfully");
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            error: "Internal Server Error",
+            message: error.message,
+        });
+    }
+};
+
+
+module.exports = { getAllCustomers, getCustomerById, updateCustomerById };
--- backend/routes/customerRouts.js
+++ backend/routes/customerRouts.js
@@ -3,9 +3,11 @@ const customerRouter = express.Router();
 const {
     getAllCustomers,
     getCustomerById,
+    updateCustomerById,
 } = require("../controllers/customers");
 
 customerRouter.get("/", getAllCustomers);
 customerRouter.get("/:id", getCustomerById);
+customerRouter.put("/update/:id", updateCustomerById);
 
 module.exports = customerRouter;
